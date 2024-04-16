8709e55ac2ecc53eae7f06bfd2b39e6c65bd0642
MIDHUN V S <114806736+vsmidhun21@users.noreply.github.com>
2024-04-16 18:38:58 +0530

    works

--- ecommerce/eshop/models.py
+++ ecommerce/eshop/models.py
@@ -1,2 +1,37 @@
 from django.db import models
+import datetime
+import os
 
+
+def getFileName(request,filename):
+    now_time=datetime.datetime.now().strftime("%y%m%d%H:%M:%S")
+    new_filename="%s%s"%(now_time,filename)
+    return os.path.join('uploads/',new_filename)
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=150,null=False,blank=False)
+    image = models.ImageField(upload_to=getFileName,null=True,blank=True)
+    description=models.TextField(max_length=500,null=False,blank=False)
+    status=models.BooleanField(default=False,help_text="0-default,1-Hidden")
+    trending=models.BooleanField(default=False,help_text="0-default,1-Trending")
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class Products(models.Model):
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    name=models.CharField(max_length=150,null=False,blank=False)
+    vendor=models.CharField(max_length=150,null=False,blank=False)
+    product_image = models.ImageField(upload_to=getFileName,null=True,blank=True)
+    quantity=models.IntegerField(null=False,blank=False)
+    original_price=models.IntegerField(null=False,blank=False)
+    selling_price=models.IntegerField(null=False,blank=False)
+    description=models.TextField(max_length=500,null=False,blank=False)
+    status=models.BooleanField(default=False,help_text="0-default,1-Hidden")
+    trending=models.BooleanField(default=False,help_text="0-default,1-Trending")
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
